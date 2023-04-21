b6a6d8c8b817a1ed8a88f974b2578a67d6e18db9
tungthanhle <lethanhtung19@gmail.com>
2023-04-21 17:21:46 +0700

    kiemtrasochanle

---
+++ index.js
@@ -0,0 +1,14 @@
+
+  
+
+  function testing() { 
+    let a = document.getElementById("test");
+  let b= a.value
+let r= b % 2 
+
+if (r==0)
+alert(`${b} la so chan`) ;
+else 
+alert(`${b} la so le`)
+    
+  }
